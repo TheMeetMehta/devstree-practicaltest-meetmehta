147b14d28f65b3524c10b4d1e75d5d48b1afc249
Meet Mehta <76428767+meet700@users.noreply.github.com>
2023-03-29 18:00:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,5 @@ const userRouter = require('./routers/users')
 const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(userRouter)
-app.get('/', async (req, res) => {
-    try {
-        await task.save()
-        res.status(200).send('Welcome to the task api')
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
